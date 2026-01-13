ad2865048229679c11775ea617aea86a3ba22eb2
saqib-rafique <saqib.rafique@arbisoft.com>
2026-01-13 22:40:15 +0500

    add: add youtube api key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -5,10 +5,10 @@ const youtube = axios.create({
 });
 
 export async function searchVideos(query, maxResults = 12) {
-  const apiKey = import.meta.env.VITE_YT_API_KEY;
+  const apiKey = import.meta.env.YT_API_KEY;
 
   if (!apiKey) {
-    throw new Error("Missing VITE_YT_API_KEY in .env");
+    throw new Error("Missing YT_API_KEY in .env");
   }
 
   const { data } = await youtube.get("/search", {
